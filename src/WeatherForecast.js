b03b6f49de148c4ca4484811f1fe4829469247d4
Ghazaal <115784408+GhazaalAsh@users.noreply.github.com>
2024-01-21 17:46:56 +0330

    Solved API Issue

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -18,16 +18,16 @@ export default function WeatherForecast(props) {
       <div className="WeatherForecast">
         <div className="row">
           <div className="col">
-            <WeatherForecastDay data={forecastData[0]} />;
+            <WeatherForecastDay data={forecastData[0]} />
           </div>
         </div>
       </div>
     );
   } else {
-    let apikey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apikey = "25fad9f7e87157d33dde0f82ab269ee8";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apikey}`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apikey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
     return null;
   }
