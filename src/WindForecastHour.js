5aacccdff1dd9eeebd3a0df89546cba6722156f9
Ghazaal <115784408+GhazaalAsh@users.noreply.github.com>
2024-01-28 13:27:23 +0330

    Wind Degree Updated

--- src/WindForecastHour.js
+++ src/WindForecastHour.js
@@ -7,8 +7,24 @@ export default function WindForecastDay(props) {
     return `${speed} km/h`;
   }
   function windDegree() {
-    let degree = Math.round(props.data.wind_deg);
-    return `${degree}`;
+    let degree = props.data.wind_deg;
+    if (degree <= 23 || degree >= 337) {
+      return "N";
+    } else if (degree >= 24 && degree <= 68) {
+      return "NE";
+    } else if (degree >= 69 && degree <= 113) {
+      return "E";
+    } else if (degree >= 114 && degree <= 158) {
+      return "SE";
+    } else if (degree >= 159 && degree <= 203) {
+      return "S";
+    } else if (degree >= 204 && degree <= 248) {
+      return "SW";
+    } else if (degree >= 249 && degree <= 293) {
+      return "W";
+    } else {
+      return "NW";
+    }
   }
   function time() {
     let date = new Date(props.data.dt * 1000);
@@ -31,9 +47,9 @@ export default function WindForecastDay(props) {
     <div ClassName="WindForecastHour">
       <div className="WindForecast-hour">{time()}</div>
       <WeatherIcon code={props.data.weather[0].icon} size={36} />
-      <div className="WindForecast-temperature">
-        <span className="WindForecast-temperature-max">{windSpeed()}</span>
-        <span className="WindForecast-temperature-min">{windDegree()}</span>
+      <div className="WindForecast-wind">
+        <span className="WindForecast-wind-speed">{windSpeed()}</span>
+        <span className="WindForecast-wind-degree">{windDegree()}</span>
       </div>
     </div>
   );
